cff1ae920b31e66f692fdffacee987297f1bcc07
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2022-07-29 15:08:13 +0300

    [feature][Salih] add routes :sparkles:

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:karpuz/pages/feed_page.dart';
+import 'package:karpuz/pages/login_page.dart';
+import 'package:karpuz/pages/post_page.dart';
+import 'package:karpuz/pages/profile_page.dart';
+import 'package:karpuz/pages/register_page.dart';
+import 'package:karpuz/utils/colors.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'Karpuz',
-    theme: ThemeData(primarySwatch: Colors.red),
-    home: const App(),
+    theme: ThemeData.dark().copyWith(
+      scaffoldBackgroundColor: backgroundColor,
+    ),
+    home: const FeedPage(),
+    debugShowCheckedModeBanner: false,
+    routes: {
+      'postPage': (_) => const PostPage(),
+      'profilePage': (_) => const ProfilePage(),
+      'loginPage': (_) => const LoginPage(),
+      'registerPage': (_) => const RegisterPage(),
+    },
   ));
-}
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Karpuz")),
-    );
-  }
 }
