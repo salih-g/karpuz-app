6e1b0607f5cd8b7a1cbfca02dae57ccd2c633dcb
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2022-07-29 15:07:15 +0300

    [feature][Salih] add color constants :sparkles:

---
+++ lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromRGBO(32, 32, 36, 1);
+const appBarColor = Color.fromRGBO(255, 255, 255, 1);
+const mainRed = Color.fromRGBO(239, 68, 67, 1);
