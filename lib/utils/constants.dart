d8dd224bbea2f0180feebe2ec0819e8a69150c9d
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2022-08-07 00:22:48 +0300

    [feature][Salih] add font and font styles ✨

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 
 const backgroundColor = Color.fromRGBO(32, 32, 36, 1);
-const appBarColor = Color.fromRGBO(255, 255, 255, 1);
 const mainRed = Color.fromRGBO(239, 68, 67, 1);
-const titleColor = Color.fromRGBO(75, 85, 100, 1);
+const textGray400 = Color.fromRGBO(156, 163, 175, 1);
+const textGray500 = Color.fromRGBO(107, 114, 128, 1);
+const textGray600 = Color.fromRGBO(75, 85, 99, 1);
 
-const appBarTitleStyle = TextStyle(color: titleColor);
+const appBarTitleStyle = TextStyle(
+    color: textGray600,
+    fontFamily: 'OpenSans',
+    fontWeight: FontWeight.bold,
+    fontSize: 20);
+
+const titleStyle = TextStyle(
+  color: textGray600,
+  fontFamily: 'OpenSans',
+  fontWeight: FontWeight.w600,
+  fontSize: 14,
+);
+
+const regularTextStyle = TextStyle(
+  color: textGray500,
+  fontFamily: 'OpenSans',
+);
+
+const thinTextStyle = TextStyle(
+  color: textGray400,
+  fontFamily: 'OpenSans',
+);
